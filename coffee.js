16bee4c884ea1a07f9caf8dde45d731d7968fb18
Mikey McCormack <michaeljohnmccormack@gmail.com>
2020-07-19 10:05:31 -0400

    Parse arrival dates into JS Date and parse scores to floats, then put the data into objects.

--- coffee.js
+++ coffee.js
@@ -22,6 +22,7 @@ async function getVisual() {
         await linkToViewAllGreenCoffees[0].click();
         await page.waitForNavigation();
         let greenCoffeeTableRows = await page.$x(coffeeTableRowsXPath);
+        let greenCoffees = [];
 
         for (let i = 0; i < greenCoffeeTableRows.length; i++) {
             let nameLink = await greenCoffeeTableRows[0].$x(coffeeNameXPath);
@@ -35,17 +36,27 @@ async function getVisual() {
                 let arrivalDateElement = await page.$x(arrivalDateXPath)
                 if (arrivalDateElement && arrivalDateElement[0]) {
                     let arrivalDate = await arrivalDateElement[0].$eval('.data', node => node.innerText.trim())
-                    console.log('Name: ', coffeeName, ' | ', 'Score: ', coffeeScore, ' | ', 'Arrived: ', arrivalDate);
+                    greenCoffees.push({name: coffeeName, score: parseFloat(coffeeScore), arrived: parseDate(arrivalDate)});
                 }
             }
             await page.goBack();
             greenCoffeeTableRows = await page.$x(coffeeTableRowsXPath);
         }
 
+        console.log(greenCoffees);
         await browser.close()
     } catch (error) {
         console.error(error)
     }
+}
+
+function parseDate(dateString) {
+    let dateTokens = dateString.split(' ');
+    const monthNumLookupTable = {
+        'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+        'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12
+    };
+    return new Date(dateTokens[1], monthNumLookupTable[dateTokens[0]], 0)
 }
 
 getVisual()
